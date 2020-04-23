655b68d2dc17086600bc4d7c7af0eab0b13ffcd5
vict1896 <victor.einfeldt.playstation@gmail.com>
2020-04-23 14:41:24 +0200

    Revert "Rød bold"
    This reverts commit 3b771a09650c4d38b2acd4642df578c8191837c5, reversing
    changes made to 1e5f4d7383d99b8540100eefcc73eaafffbdb10b.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,26 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //  public CharacterController controller;
-
-    Rigidbody rb;
+    public CharacterController controller;
 
     public float speed = 12f;
 
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
 
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal") * (speed * Time.deltaTime);
-        float z = Input.GetAxis("Vertical") * (speed * Time.deltaTime);
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
-        Vector3 move = new Vector3(x, rb.velocity.y, z); 
+        Vector3 move = transform.right * x + transform.forward * z;
 
-        rb.velocity = move;
-       // controller.Move(move * speed * Time.deltaTime);
+        controller.Move(move * speed * Time.deltaTime);
     }
 }
