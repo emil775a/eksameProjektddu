5ee0fdb0cd6aaf25ac37fe189cc0f8df177bc6d9
TandruptT <56020414+TandruptT@users.noreply.github.com>
2020-04-23 13:33:24 +0200

    Controller movement (Virker)

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController controller;
+    //  public CharacterController controller;
+
+    Rigidbody rb;
 
     public float speed = 12f;
 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal") * (speed * Time.deltaTime);
+        float z = Input.GetAxis("Vertical") * (speed * Time.deltaTime);
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        Vector3 move = new Vector3(x, rb.velocity.y, z); 
 
-        controller.Move(move * speed * Time.deltaTime);
+        rb.velocity = move;
+       // controller.Move(move * speed * Time.deltaTime);
     }
 }
