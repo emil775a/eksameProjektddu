be834268a0a1f50a4a84106c7878c3d902858e49
emil775a <56863437+emil775a@users.noreply.github.com>
2020-05-09 12:08:02 +0200

    grow

---
+++ Assets/stript/grow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space")){
+            transform.localScale = new Vector3 (Random.Range(0.2f, 5.0f), 1, Random.Range(0.2f, 5.0f));
+        }
+           
+    }
+}
