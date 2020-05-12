66af20f0a9acd4863936c1b30e8e669a2f7b5063
emil775a <56863437+emil775a@users.noreply.github.com>
2020-05-12 17:25:09 +0200

    mass for door

---
+++ Assets/stript/massforDoor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class massforDoor : MonoBehaviour
+{
+
+    public float mass;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider col)
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.mass = mass;
+    }
+}
