51845f7f4d2da6008b0766658fe8ef5ebdb09e22
vict1896 <victor.einfeldt.playstation@gmail.com>
2020-05-13 13:00:02 +0200

    Frimse skifter farver

---
+++ Assets/stript/BallTriggerEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallTriggerEvent : MonoBehaviour
+{
+
+    [SerializeField]
+    public GameObject target;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        target.GetComponent<Renderer>().material.color = Color.green;
+    }
+}
